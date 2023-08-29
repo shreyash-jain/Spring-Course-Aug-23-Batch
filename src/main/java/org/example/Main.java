5f5bb52d6109db7fbd6008c747970ab43c4542be
Shreyash <shreyashjain1996@gmail.com>
2023-08-29 13:47:29 +0530

    Project using MAVEN library

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import MyPackage.ClassB;
+import MyPackage.src.main.java.ClassC;
+
+public class Main {
+    ClassC classA = new ClassC();
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
