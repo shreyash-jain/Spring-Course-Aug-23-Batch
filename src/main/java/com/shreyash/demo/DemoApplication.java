bdb80b7625ee998dcb61a3b39c846f62d8f3ca49
Shreyash <shreyashjain1996@gmail.com>
2023-10-08 08:47:08 +0530

    added hibernate db

--- src/main/java/com/shreyash/demo/DemoApplication.java
+++ src/main/java/com/shreyash/demo/DemoApplication.java
@@ -63,68 +63,10 @@ public class DemoApplication implements ApplicationRunner {
 		Notification n = applicationContext.getBean(Notification.class);
 		n.sendMessage("Hello");
 
-		//persistData();
-
 		persistWithHibernate();
 	}
 
-//	public void persistWithJPA(){
-//		// JPA - Hibernate
-//		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
-//		// Persistence Context
-//		EntityManager entityManager = entityManagerFactory.createEntityManager();
-//		//
-//
-//		System.out.println("Starting Transaction");
-//		entityManager.getTransaction().begin();
-//		Restaurants restaurant = new Restaurants();
-//		// New - Transcient
-//
-//		restaurant.setName("Sagar Ratna 2");
-//		restaurant.setRating(4);
-//		System.out.println("Saving restaurant to Database");
-//
-//		entityManager.persist(restaurant);
-//		// I have reached the managed state
-//
-//		entityManager.detach(restaurant);
-//		// change the state to detach state
-//
-//		entityManager.merge(restaurant);
-//
-//		restaurant.setPincode(1223);
-//		// still managed state
-//		// Managed
-//		entityManager.getTransaction().commit();
-//		System.out.println("Generated restaurant ID = " + restaurant.getId());
-//
-//		// get an object using primary key.
-//		Restaurants res = entityManager.find(Restaurants.class, restaurant.getId());
-//		System.out.println("Got object " + res.getName() + " " + res.getId());
-//
-//		// get all the objects from restaurant table
-//		@SuppressWarnings("unchecked")
-//		Query listResult = entityManager.createQuery("SELECT e FROM Restaurants e");
-//		List<Restaurants> listRes = listResult.getResultList();
-//
-//		if (listRes == null) {
-//			System.out.println("No Restaurant found . ");
-//		} else {
-//			for (Restaurants r :  listRes) {
-//				System.out.println("Restaurants name= " + r.getName() + ", Employee id " + r.getId());
-//			}
-//		}
-//		// remove and entity
-//		entityManager.getTransaction().begin();
-//		System.out.println("Deleting Restaurant with ID = " + res.getId());
-//		entityManager.remove(res);
-//		entityManager.getTransaction().commit();
-//
-//		// close the entity manager
-//		entityManager.close();
-//		entityManagerFactory.close();
-//
-//	}
+
 
 	// Application
 	// JPA -> JPA, Native Hibernate -> ORM -> EntityManager -> Repositories
@@ -191,35 +133,6 @@ public class DemoApplication implements ApplicationRunner {
 	}
 
 
-	public void persistData() {
-
-		try {
-
-			java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/swiggy", "foo", "bar");
-
-			PreparedStatement stmt = connection.prepareStatement("INSERT INTO PUBLISHER (CODE, PUBLISHER_NAME) VALUES (?, ?)");
-			stmt.setString(1, "4");
-			stmt.setString(2,"2");
-			stmt.executeUpdate();
-
-			stmt.close();
-
-			stmt = connection.prepareStatement("INSERT INTO PUBLISHER (ISBN, BOOK_NAME, PUBLISHER_CODE) VALUES (?, ?, ?)");
-			stmt.setString(1,"5");
-			stmt.setString(2, "2");
-			stmt.setString(3, "3");
-			stmt.executeUpdate();
-
-			// connection pool
-
-			//Todo t = Todo.builder().build();
-			//t.getResource();
-
-			stmt.close();
-
-		}
-		catch (SQLException e) { e.printStackTrace(); } finally { try { connection.close(); } catch (SQLException e) { e.printStackTrace(); } }
-	}
 
 
 }
